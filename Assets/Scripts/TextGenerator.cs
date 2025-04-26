0e545786f502fb5743d9a3ba05e1067e1a68117b
wswfws <wswfws@ya.ru>
2025-04-27 02:53:15 +0500

    мяу

--- Assets/Scripts/TextGenerator.cs
+++ Assets/Scripts/TextGenerator.cs
@@ -2,21 +2,60 @@ using System;
 using TMPro;
 using UnityEngine;
 using System.Collections.Generic;
+using Random = System.Random;
+
+public class News
+{
+    public string text;
+    public Dictionary<string, string> incorrectWords;
+
+    public News(string text, Dictionary<string, string> incorrectWords)
+    {
+        this.text = text;
+        this.incorrectWords = incorrectWords;
+    }
+}
+
+public static class NewsLibrary
+{
+    public static readonly List<News> news = new()
+    {
+        new News(
+            "Тело женщины с наживasfdasыми ранениями нашли на железывадорожный станции в Екатеринбурге",
+            new Dictionary<string, string>
+            {
+                ["наживasfdasыми"] = "ножевыми",
+                ["железывадорожный"] = "железнодорожной"
+            }),
+        new News(
+            "Тело женщины с наживasfdasыми ранениями нашли на железывадорожный станции в Екатеринбурге",
+            new Dictionary<string, string>
+            {
+                ["наживasfdasыми"] = "ножевыми",
+                ["железывадорожный"] = "железнодорожной"
+            })
+    };
+
+    public static News GetRandomNews()
+    {
+        var random = new Random();
+        var rndInd = random.Next(news.Count);
+        return news[rndInd];
+    }
+}
 
 public class TextGenerator : MonoBehaviour
 {
     public TextMeshProUGUI text;
-    private static Dictionary<string, string> incorrectWords = new()
-    {
-        ["наживasfdasыми"] = "ножевыми",
-        ["железывадорожный"] = "железнодорожной"
-    };
+    private static Dictionary<string, string> incorrectWords;
 
     private static HashSet<string> goodWords = new();
 
     void Start()
     {
-        text.text = "Тело женщины с наживasfdasыми ранениями нашли на железывадорожный станции в Екатеринбурге";
+        var news = NewsLibrary.GetRandomNews();
+        incorrectWords = news.incorrectWords;
+        text.text = news.text;
         foreach (var word in goodWords)
         {
             text.text = text.text.Replace(word, incorrectWords[word]);
@@ -39,6 +78,7 @@ public class TextGenerator : MonoBehaviour
                 gameObject.AddComponent<Load2Game>().CallLoad2Game();
                 break;
         }
+
         EnergyController.DestroyTimer();
     }
 }
