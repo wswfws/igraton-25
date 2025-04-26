107779829261f8a0787607cc030b49acce80a2ee
CtREL0K <Kzuev905@gmail.com>
2025-04-26 18:27:23 +0500

    3d mini-game

---
+++ Assets/Scripts/3taskButton.cs
@@ -0,0 +1,68 @@
+using System;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System.Collections;
+using Random = UnityEngine.Random;
+
+public class ClickClickGame : MonoBehaviour
+{
+    public Button targetButton;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI timerText;
+    public int maxTimer;
+    private int score;
+    private bool isEnd;
+    private float timer;
+    
+
+    void Start()
+    {
+        targetButton.onClick.AddListener(OnButtonClick);
+        StartCoroutine(MoveButtonRoutine());
+        scoreText.text = "Clicks: " + score + "/10";
+    }
+
+    void Update()
+    {
+        if (!isEnd)
+        {
+            timer += Time.deltaTime;
+            if (timer >= maxTimer) EndGame(false);
+            timerText.text = "Times remaining: " + Mathf.Ceil(maxTimer - timer) + " sec.";
+        }
+    }
+
+    private void OnButtonClick()
+    {
+        if (isEnd) return;
+        score++;
+        scoreText.text = "Clicks: " + score + "/10";
+        if (score >= 10)
+        {
+            EndGame(true);
+        }
+
+        var screenWidth = Screen.width;
+        var screenHeight = Screen.height;
+        var newPosition = new Vector3(Random.Range(100f, screenWidth - 100f), Random.Range(50f, screenHeight - 50f), 0);
+        targetButton.transform.position = newPosition;
+    }
+
+    private IEnumerator MoveButtonRoutine()
+    {
+        while (score < 10)
+        {
+            Vector3 shakeOffset = new Vector2(Random.Range(-3, 3), Random.Range(-3, 3));
+            targetButton.transform.position += shakeOffset;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    private void EndGame(bool isWin)
+    {
+        isEnd = true;
+        timerText.text = isWin ? "Congrats! You win!" : "Times up! You loose!";
+        throw new NotImplementedException("Добавить возвращение на гл сцену");
+    }
+}
